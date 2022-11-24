7bbd6680181f47cd26872353aec91ed1d3d1afa1
NukkiZip <coolman80001@gmail.com>
2022-11-24 09:57:58 -0500

    Done HauntedCarnival (maybe)

--- ICS4UCarnivalProject/src/HauntedCarnival.java
+++ ICS4UCarnivalProject/src/HauntedCarnival.java
@@ -1,5 +1,63 @@
 
+import java.util.*;
+
 public class HauntedCarnival {
 
-	private int money;
+	private double money;
+	private int tickets;
+	private String[] inventory;
+	
+	public HauntedCarnival(int money1, String[] inventory1) {
+		
+		money = money1;
+		tickets = 0;
+		inventory = inventory1;
+	}
+	
+	public double getMoney() {
+		
+		return money;
+	}
+	
+	public int getTickets() {
+		
+		return tickets;
+	}
+	
+	public String[] getInventory() {
+		
+		return inventory;
+	}
+	
+	public void convertToTickets() {
+		
+		Scanner input = new Scanner(System.in);
+		int howmuch;
+		
+		System.out.println("How much money do you want to convert? (You currently have " + money + ")");
+		howmuch = input.nextInt();
+		
+		do {
+		
+		if(howmuch <= money) {
+		
+			tickets = tickets + (howmuch*20);
+			money = money - howmuch;
+			System.out.println("You now have " + tickets + " tickets.");
+			break;
+		
+		} else {
+		
+			do {
+				
+				System.out.println("You have entered the number that exceeded your amount of money, please try again.");
+				howmuch = input.nextInt();
+			
+			}while(howmuch > money);
+		}
+		
+		}while(howmuch < money);
+		
+		input.close();
+	}
 }
---
+++ ICS4UCarnivalProject/src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] aa = new String[10];
+		int money = 100;
+		HauntedCarnival a = new HauntedCarnival(money, aa);
+		
+		a.convertToTickets();
+		a.convertToTickets();
+	}
+
+}
