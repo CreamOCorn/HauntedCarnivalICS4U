9d3233a6aa855c3e515cb738569e6a5f06c97d01
CreamOCorn <smileysundae3@gmail.com>
2022-11-25 09:10:07 -0500

    Added a menu and introduction main class

--- ICS4UCarnivalProject/src/PlayerMain.java
+++ ICS4UCarnivalProject/src/PlayerMain.java
@@ -4,31 +4,67 @@ public class PlayerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String userChoice = "0";
 		Scanner input = new Scanner(System.in);
+		//printIntro(input);
 		
-		System.out.println("You exit your car on a misty October night...");
-		input.nextLine();
-		System.out.println("The fog surrounds you, concealing the ground beneath your feet...");
-		input.nextLine();
-		System.out.println("The lights call to you through the thick of the smoke. You feel drawn to it...");
-		input.nextLine();
-		System.out.println("You get closer... The flourescent bulbs flicker.");
-		input.nextLine();
-		System.out.println("The shine of the billboard sign calls to you. When you make it past the fog, you look up to see five words in bold letters before you...");
-		input.nextLine();
-		System.out.println("\"WELCOME TO THE HAUNTED CARNIVAL!\"");
-		input.nextLine();
-		System.out.println("You enter the gate, greeted by ghosts, ghouls, and witches all around. Attractions left and right all catch your eye.");
-		input.nextLine();
+		while (!userChoice.equals("9")) { 
+			System.out.println("-----------------------------------------------");
+			System.out.println("Where would you like to go now?\n"
+					+ "1) Roulette\n"
+					+ "2) Graveyard Dig\n"
+					+ "3) Haunted Maze\n"
+					+ "4) Devilish Diner\n"
+					+ "5) Prize Stand\n"
+					+ "9) Leave the carnival and go home");
+			input = new Scanner (System.in);
+			userChoice = (input.nextLine());
+			System.out.println();
+			
+			if (userChoice.equals("1") || userChoice.equalsIgnoreCase("Roulette")) {
+				System.out.println("You make your way to the roulette stand. [Press Enter to Continue]");
+				input.nextLine();
+			} else if (userChoice.equals("2") || userChoice.equalsIgnoreCase("Graveyard Dig")) {
+				System.out.println("The graveyeard digging site. [Press Enter to Continue]");
+				input.nextLine();
+			} else if (userChoice.equals("3") || userChoice.equalsIgnoreCase("Haunted Maze")) {
+				System.out.println("The sounds of screams from the hounted maze intruiges you. [Press Enter to Continue]");
+				input.nextLine();
+			} else if (userChoice.equals("4") || userChoice.equalsIgnoreCase("Devilish Diner")) {
+				System.out.println("Your stomach grumbles as you go to the Devilish Diner for a quick bite. [Press Enter to Continue]");
+				input.nextLine();
+			} else if (userChoice.equals("5") || userChoice.equalsIgnoreCase("Prize Stand")) {
+				System.out.println("A huge stand showcasing plushies, toys, and candies lights up the whole park. [Press Enter to Continue]");
+				input.nextLine();
+			} else if (userChoice.equals("9") || userChoice.toLowerCase().contains("leave") || userChoice.toLowerCase().contains("home")) {
+				System.out.println("You stretch as you decide to wrap up for the day. [Press Enter to Continue]");
+				input.nextLine();
+				System.out.println("Waving to all of the kind monsters, you exit the gate. [Press Enter to Continue]");
+				input.nextLine();
+			} else {
+				System.out.println("That attraction doesn't seem to exist. You look around again.");
+			}
+		}
 		
-		System.out.println("Where would you like to go?\n"
-							+ "1) Roulette\n"
-							+ "2) Graveyard Dig\n"
-							+ "3) Haunted Maze\n"
-							+ "4) Devilish Diner\n"
-							+ "5) Prize Stand\n"
-							+ "9) Leave the carnival and go home");
+	}
+	
+	public static void printIntro(Scanner input) {
+		System.out.println("You exit your car on a misty October night... [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("The fog surrounds you, concealing the ground beneath your feet... [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("The lights calls to you through the thick of the smoke. You feel drawn to it... [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("The flourescent bulbs of the billboard sign flicker... [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("You look up to see five words in bold letters before you... [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("\"WELCOME TO THE HAUNTED CARNIVAL!\" [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("You enter the gate, greeted by ghosts, ghouls, and witches all around. [Press Enter to Continue]");
+		input.nextLine();
+		System.out.println("Attractions left and right all catch your eye. [Press Enter to Continue]");
+		input.nextLine();
 	}
 
 }
